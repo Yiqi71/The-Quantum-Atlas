80f3b8735b70a8313bff20a6c70cb993226be476
Yiqi71 <yiqi_c@icloud.com>
2025-02-08 16:42:51 -0500

    1

--- panel3.js
+++ panel3.js
@@ -1,12 +1,12 @@
 // panel3
 let panel3 = document.getElementById(`panel3`);
-let pngNames = ["atom", "atoms", "background", "lock"];
+let pngNames = ["atom1","atom2", "atoms", "bg1", "bg2", "lockOutline","lockFill", "outline"];
 
 
 for (let i = 0; i < pngNames.length; i++) {
     let new_img = document.createElement("img");
-    new_img.src = "panelsImg/color_3/" + pngNames[i] + ".png";
-    new_img.alt = "panelsImg/color_3/" + pngNames[i] + ".png";
+    new_img.src = "panelsImg/panel3/" + pngNames[i] + ".png";
+    new_img.alt = "panelsImg/panel3/" + pngNames[i] + ".png";
     new_img.width *= scaling;
     new_img.style.position = "absolute";
     new_img.style.left = "0";
@@ -24,7 +24,7 @@ circleContainer.style.top = "70px";
 circleContainer.style.left = "20px";
 circleContainer.style.width = "97px";
 circleContainer.style.height = "90px";
-let circles = ["bigger_circle", "smaller_circle"];
+let circles = ["bigCircle", "smallCircle"];
 let circleTops = [0, 25];
 let circleLefts = [0, 30];
 let circleScaling = [0.70, 0.7];
@@ -32,9 +32,11 @@ let zIndexs = [-10,-1];
 for (let i = 0; i < circles.length; i++) {
     let new_circle = document.createElement("img");
     new_circle.id = circles[i];
-    new_circle.src = "panelsImg/color_3/" + circles[i] + ".png";
-    new_circle.alt = "panelsImg/color_3/" + circles[i] + ".png";
+    new_circle.src = "panelsImg/panel3/" + circles[i] + ".png";
+    new_circle.alt = "panelsImg/panel3/" + circles[i] + ".png";
     new_circle.width *= scaling;
+    new_circle.style.top = circleTops[i];
+    new_circle.style.left = circleLefts[i];
 
     new_circle.style.position = "absolute";
 
